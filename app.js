e58d21fa19bc86fdbea04ba7cb2a77cf452625e2
Abdul Razaq <abdulrazaqramdurg006@gmail.com>
2026-08-28 10:23:19 +0530

    Add category filter button listeners and dynamic grid rendering

--- app.js
+++ app.js
@@ -48,3 +48,25 @@ const products = [
     inStock: true
   }
 ];
+
+const main = document.getElementById("productGrid");
+
+
+
+function add_products(products) {
+    products.forEach(product => {
+        main.innerHTML = "";
+
+        main.innerHTML += `
+        <div class = "product_card">
+            <img src="${product.image}" alt="${product.name}">
+            <p>${product.name}</p>
+            <p>${product.category}</p>
+            <p>${product.price.toFixed(2)}</p>
+            <button class = "add_to_cart_button" data-id = "${product.id}" ${!product.inStock ? 'disabled' : ''}>${!product.inStock ? 'Out Of Stock' : 'Add to Cart'}</button>
+        </div>
+        `
+    });
+}
+
+add_products(products);
